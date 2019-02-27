c5cad925c371bf73c75c0b890e301282336545b7
Scott Wales <scott.wales@unimelb.edu.au>
2019-02-27 16:46:52 +1100

    Add report test

---
+++ test/test_report.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# Copyright 2019 ARC Centre of Excellence for Climate Extremes
+# author: Scott Wales <scott.wales@unimelb.edu.au>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import print_function
+
+from dusql.report import report
+
+from dusql import model
+import sqlalchemy as sa
+
+def test_report(conn, sample_db, sample_data):
+    report(sample_data, conn)
+
+
+def test_autoscan(conn, sample_data):
+    report(sample_data, conn)
+
+    q = sa.select([model.paths.c.inode])
+    r = conn.execute(q)
+    assert len(list(r)) > 0
