1e98a3fb4e5d996afa6e1fb8a8951915367be635
Scott Wales <scott.wales@unimelb.edu.au>
2019-03-14 14:06:27 +1100

    Adding some checks

---
+++ src/dusql/check.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+#
+# Copyright 2019 Scott Wales
+#
+# Author: Scott Wales <scott.wales@unimelb.edu.au>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .find import find_children
+from . import model
+
+import sqlalchemy as sa
+import stat
+import grp
+
+
+class Check:
+    pass
+
+
+class Group(Check):
+    """
+    Check all directories under root_ids are owned by a specific group
+    """
+    name = 'group'
+
+    def __init__(self, group):
+        if isinstance(group, int):
+            self.group = group
+        else:
+            self.group = grp.getgrnam(group).gr_gid
+
+    def query(self, root_ids, connection=None):
+        return (find_children(root_ids)
+                .where(~(model.paths.c.gid == self.group)))
+
+
+
+class DirectoryGroupReadable(Check):
+    """
+    Check all directories under root_ids are group_readable
+    """
+    name = 'directory-group-readable'
+
+    def query(self, root_ids, connection=None):
+        return (find_children(root_ids)
+                .where(model.paths.c.mode.op('&')(stat.S_IFDIR))
+                .where(~(model.paths.c.mode.op('&')(stat.S_IRGRP | stat.S_IXGRP)))
+                )
+
+
+class NetCDFCompression(Check):
+    """
+    Check NetCDF files are compressed
+    """
+    name = 'netcdf-compression'
+
+    def __init__(self, min_size, filename_pattern='*.nc'):
+        self.min_size = min_size
+        self.filename_pattern = filename_pattern
+
+    def query(self, root_ids, connection):
+        netcdf_files = (find_children(root_ids)
+                .where(model.paths.c.size > self.min_size)
+                .where(model.paths.c.mode.op('&')(stat.S_IFREG))
+                .where(model.paths.c.name.op('GLOB')(self.filename_pattern))
+                .alias('netcdf_files')
+                )
+
+        netcdf_paths = (
+                sa.select([model.paths_fullpath.c.path_id, model.paths_fullpath.c.path])
+                .select_from(
+                    model.paths_fullpath
+                    .join(netcdf_files, netcdf_files.c.id == model.paths_fullpath.c.path_id))
+                )
+
+        found_ids = []
+        for r in connection.execute(netcdf_paths):
+            if self.check_file(r.path):
+                found_ids.append(r.path_id)
+
+        return sa.select([model.paths.c.id]).where(model.paths.c.id.in_(found_ids))
+
+    def check_file(self, path):
+        return True
---
+++ test/test_checks.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#
+# Copyright 2019 Scott Wales
+#
+# Author: Scott Wales <scott.wales@unimelb.edu.au>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dusql.check import *
+from dusql.scan import scan
+from dusql.handler import get_path_id
+
+def test_directory_group_readable(conn, tmp_path):
+    a = tmp_path / 'a'
+    a.mkdir()
+
+    # Directory is group readable
+    a.chmod(0o740)
+    scan(tmp_path, conn)
+    root_id = get_path_id(tmp_path, conn)
+    r = DirectoryGroupReadable().query([root_id])
+    assert len(list(conn.execute(r))) == 0
+
+    # Directory is not group readable
+    a.chmod(0o700)
+    scan(tmp_path, conn)
+    r = DirectoryGroupReadable().query([root_id])
+    assert len(list(conn.execute(r))) == 1
+
+
+def test_netcdf_compression(conn, tmp_path):
+    a = tmp_path/'a.nc'
+    a.write_text('test')
+    
+    scan(tmp_path, conn)
+    root_id = get_path_id(tmp_path, conn)
+
+    c = NetCDFCompression(min_size = 0)
+    r = c.query([root_id], conn)
+    assert len(list(conn.execute(r))) == 1
+
