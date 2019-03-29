01cda9ea7e838a1795a0fc59081def568fbb9195
Scott Wales <scott.wales@unimelb.edu.au>
2019-03-29 15:52:33 +1100

    Comment out unfinished netcdf test

--- src/dusql/check.py
+++ src/dusql/check.py
@@ -141,42 +141,42 @@ class DirectoryGroupReadable(Check):
                 )
 
 
-class NetCDFCompression(Check):
-    """
-    Check NetCDF files are compressed
-    """
-    name = 'netcdf-compression'
-
-    def __init__(self, min_size, filename_pattern='*.nc'):
-        self.min_size = min_size
-        self.filename_pattern = filename_pattern
-
-    def query(self, root_ids, connection):
-        children = find_children(root_ids)
-        netcdf_files = (children
-                .select_from(
-                    children
-                    .join(model.basenames, model.paths.c.basename_id == model.basenames.c.id)
-                    )
-                .where(model.paths.c.size > self.min_size)
-                .where(model.paths.c.mode.op('&')(stat.S_IFREG))
-                .where(model.basenames.c.name.op('GLOB')(self.filename_pattern))
-                .alias('netcdf_files')
-                )
-
-        netcdf_paths = (
-                sa.select([model.paths_fullpath.c.path_id, model.paths_fullpath.c.path])
-                .select_from(
-                    model.paths_fullpath
-                    .join(netcdf_files, netcdf_files.c.id == model.paths_fullpath.c.path_id))
-                )
-
-        found_ids = []
-        for r in connection.execute(netcdf_paths):
-            if self.check_file(r.path):
-                found_ids.append(r.path_id)
-
-        return sa.select([model.paths.c.id]).where(model.paths.c.id.in_(found_ids))
-
-    def check_file(self, path):
-        return True
+# class NetCDFCompression(Check):
+#     """
+#     Check NetCDF files are compressed
+#     """
+#     name = 'netcdf-compression'
+# 
+#     def __init__(self, min_size, filename_pattern='*.nc'):
+#         self.min_size = min_size
+#         self.filename_pattern = filename_pattern
+# 
+#     def query(self, root_ids, connection):
+#         children = find_children(root_ids)
+#         netcdf_files = (children
+#                 .select_from(
+#                     children
+#                     .join(model.basenames, model.paths.c.basename_id == model.basenames.c.id)
+#                     )
+#                 .where(model.paths.c.size > self.min_size)
+#                 .where(model.paths.c.mode.op('&')(stat.S_IFREG))
+#                 .where(model.basenames.c.name.op('GLOB')(self.filename_pattern))
+#                 .alias('netcdf_files')
+#                 )
+# 
+#         netcdf_paths = (
+#                 sa.select([model.paths_fullpath.c.path_id, model.paths_fullpath.c.path])
+#                 .select_from(
+#                     model.paths_fullpath
+#                     .join(netcdf_files, netcdf_files.c.id == model.paths_fullpath.c.path_id))
+#                 )
+# 
+#         found_ids = []
+#         for r in connection.execute(netcdf_paths):
+#             if self.check_file(r.path):
+#                 found_ids.append(r.path_id)
+# 
+#         return sa.select([model.paths.c.id]).where(model.paths.c.id.in_(found_ids))
+# 
+#     def check_file(self, path):
+#         return True
