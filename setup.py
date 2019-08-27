aa1844299c09807f177406f35b3949794860fa09
Scott Wales <scott.wales@unimelb.edu.au>
2019-08-27 11:41:43 +1000

    Add entry point

--- setup.py
+++ setup.py
@@ -6,4 +6,9 @@ import versioneer
 setup(
         version=versioneer.get_version(),
         cmdclass=versioneer.get_cmdclass(),
+        entry_points={
+            'console_scripts': [
+                'dusql = grafanadb.cli:main',
+                ]
+            }
         )
