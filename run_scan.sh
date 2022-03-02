0148d6843ce74484f15cfef8f3377233818183ec
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2022-03-02 17:08:39 +1100

    Remove w35 and w48

--- run_scan.sh
+++ run_scan.sh
@@ -18,8 +18,7 @@ module load conda
 
 #set -x
 
-PROJECTS="p66 hh5/tmp v45 w35 w40 w42 w48 w97 ly62"
-#PROJECTS="w35"
+PROJECTS="p66 hh5/tmp v45 w40 w42 w97 ly62"
 
 sed -e 's:\<\(\S\+\):/g/data/\1 /scratch/\1:g' -e 's:\s\+:\n:g' <<< $PROJECTS | parallel -v --jobs 4 python src/grafanadb/dusql_scan.py {} --output $TMPDIR/'dusql.{= $_=~ s:/:_:g =}.csv'
 
